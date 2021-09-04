fc769f23f688e436c9142f48074625e7d0175b90
Christopher Bocardo <ccb012100@users.noreply.github.com>
2021-09-04 18:21:24 -0500

    start tracking dotfiles in chezmoi

---
+++ aliases.cmd
@@ -0,0 +1,13 @@
+@echo off
+
+doskey ls=dir
+
+doskey cdh=cd %USERPROFILE%
+doskey tmp=cd %TMP%
+
+doskey cb=cargo build $*
+doskey cc=cargo check $*
+doskey cr=cargo run $*
+
+doskey gb=go build .
+doskey gr=go run .
