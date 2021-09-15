db964ebd66f40a53932c76cc966bc0d54b04c002
Christopher Bocardo <ccb012100@users.noreply.github.com>
2021-09-15 15:13:47 -0500

    update copy .bat file

--- copydotfiles.bat
+++ copydotfiles.bat
@@ -8,6 +8,7 @@ REM /L -> dry run
 
 Set DOTFILES="%HOMEPATH%\.dotfiles\HOMEPATH"
 
+REM ~
 xcopy %HOMEPATH%\.bashrc %DOTFILES%\ /D /F /H /I
 xcopy %HOMEPATH%\.gitconfig %DOTFILES%\ /D /F /H /I
 xcopy %HOMEPATH%\.minttyrc %DOTFILES%\ /D /F /H /I
@@ -15,11 +16,39 @@ xcopy %HOMEPATH%\_gvimrc %DOTFILES%\ /D /F /H /I
 xcopy %HOMEPATH%\_vimrc %DOTFILES%\ /D /F /H /I
 xcopy %HOMEPATH%\aliases.cmd %DOTFILES%\ /D /F /H /I
 xcopy %HOMEPATH%\.config\nvim\init.vim %DOTFILES%\.config\nvim\ /D /F /H /I
+
+REM VS Code
 xcopy %APPDATA%\Code\User\keybindings.json %DOTFILES%\AppData\Roaming\Code\User\ /D /F /H /I
 xcopy %APPDATA%\Code\User\settings.json %DOTFILES%\AppData\Roaming\Code\User\ /D /F /H /I
+
+REM NotePad++
 xcopy %APPDATA%\NotePad++\shortcuts.xml %DOTFILES%\AppData\Roaming\NotePad++\ /D /F /H /I
+
+REM Visual Studio
 xcopy %HOMEPATH%\AppData\Local\Microsoft\VisualStudio\16.0_df333996\Settings\CurrentSettings.vssettings ^
 	%DOTFILES%\AppData\Local\Microsoft\VisualStudio\16.0_df333996\Settings\ /D /F /H /I
+
+REM Windows Terminal
 xcopy %HOMEPATH%\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json ^
 	%DOTFILES%\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\ /D /F /H /I
+
+REM Power Toys
+xcopy %HOMEPATH%\AppData\Local\Microsoft\PowerToys\settings.json ^
+	%DOTFILES%\AppData\Local\Microsoft\PowerToys\ /D /F /H /I
+xcopy %HOMEPATH%\AppData\Local\Microsoft\PowerToys\FancyZones\settings.json ^
+	%DOTFILES%\AppData\Local\Microsoft\PowerToys\FancyZones\ /D /F /H /I
+xcopy %HOMEPATH%\AppData\Local\Microsoft\PowerToys\File Explorer\settings.json ^
+	%DOTFILES%\AppData\Local\Microsoft\PowerToys\File Explorer\ /D /F /H /I
+xcopy %HOMEPATH%\AppData\Local\Microsoft\PowerToys\File Explorer\settings.json ^
+	%DOTFILES%\AppData\Local\Microsoft\PowerToys\File Explorer\ /D /F /H /I
+xcopy %HOMEPATH%\AppData\Local\Microsoft\PowerToys\Keyboard Manager\default.json ^
+	%DOTFILES%\AppData\Local\Microsoft\PowerToys\Keyboard Manager\ /D /F /H /I
+xcopy %HOMEPATH%\AppData\Local\Microsoft\PowerToys\PowerRename\power-rename-settings.json ^
+	%DOTFILES%\AppData\Local\Microsoft\PowerToys\PowerRename\ /D /F /H /I
+xcopy %HOMEPATH%\AppData\Local\Microsoft\PowerToys\PowerRename\power-rename-ui-flags ^
+	%DOTFILES%\AppData\Local\Microsoft\PowerToys\PowerRename\ /D /F /H /I
+xcopy %HOMEPATH%\AppData\Local\Microsoft\PowerToys\PowerToys Run\settings.json ^
+	%DOTFILES%\AppData\Local\Microsoft\PowerToys\PowerToys Run\ /D /F /H /I
+xcopy %HOMEPATH%\AppData\Local\Microsoft\PowerToys\PowerToys Run\Settings\PowerToysRunSettings.json ^
+	%DOTFILES%\AppData\Local\Microsoft\PowerToys\PowerToys Run\Settings\ /D /F /H /I
 @pause
