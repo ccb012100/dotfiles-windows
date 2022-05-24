42d9711262829dc457b3e6b5ba4c167f2a0279a4
Christopher Bocardo <ccb012100@users.noreply.github.com>
2022-05-24 16:38:44 -0500

    git: add ignore; fix attributes location
    added a global gitignore file
    
    fixed the location of the global attributes file and deleted extra copy
    in repo

--- update-local-from-repo.bat
+++ update-local-from-repo.bat
@@ -18,9 +18,10 @@ xcopy %DOTFILES_HOMEPATH%\_vimrc %HOMEPATH%\ /D /F /H /I
 xcopy %DOTFILES_HOMEPATH%\aliases.cmd %HOMEPATH%\ /D /F /H /I
 
 REM ~/.config
-xcopy %DOTFILES_HOMEPATH%\.config\git\attributes %HOMEPATH%\ /D /F /H /I
-xcopy %DOTFILES_HOMEPATH%\.config\nvim\init.vim %HOMEPATH%\.config\nvim\ /D /F /H /I
 xcopy %DOTFILES_HOMEPATH%\.config\liquidpromptrc %HOMEPATH%\.config\ /D /F /H /I
+xcopy %DOTFILES_HOMEPATH%\.config\git\attributes %HOMEPATH%\ /D /F /H /I
+xcopy %DOTFILES_HOMEPATH%\.config\git\ignore %HOMEPATH%\ /D /F /H /I
+xcopy %DOTFILES_HOMEPATH%\.config\nvim\init.vim %HOMEPATH%\.config\nvim\ /D /F /H /I
 
 REM ~/.gnupg
 xcopy %DOTFILES_HOMEPATH%\.gnupg\gpg.conf %HOMEPATH%\.gnupg\ /D /F /H /I
--- update-repo-from-local.bat
+++ update-repo-from-local.bat
@@ -18,9 +18,10 @@ xcopy %HOMEPATH%\_vimrc %DOTFILES_HOMEPATH%\ /D /F /H /I
 xcopy %HOMEPATH%\aliases.cmd %DOTFILES_HOMEPATH%\ /D /F /H /I
 
 REM ~/.config
-xcopy %HOMEPATH%\.config\git\attributes %DOTFILES_HOMEPATH%\ /D /F /H /I
-xcopy %HOMEPATH%\.config\nvim\init.vim %DOTFILES_HOMEPATH%\.config\nvim\ /D /F /H /I
 xcopy %HOMEPATH%\.config\liquidpromptrc %DOTFILES_HOMEPATH%\.config\ /D /F /H /I
+xcopy %HOMEPATH%\.config\git\attributes %DOTFILES_HOMEPATH%\git\ /D /F /H /I
+xcopy %HOMEPATH%\.config\git\ignore %DOTFILES_HOMEPATH%\git\ /D /F /H /I
+xcopy %HOMEPATH%\.config\nvim\init.vim %DOTFILES_HOMEPATH%\.config\nvim\ /D /F /H /I
 
 REM ~/.gnupg
 xcopy %HOMEPATH%\.gnupg\gpg.conf %DOTFILES_HOMEPATH%\.gnupg\ /D /F /H /I
