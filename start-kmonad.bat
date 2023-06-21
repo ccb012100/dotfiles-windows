8c3af4b647a05b99f4ac9822b3e84d65978d0304
Christopher Bocardo <397636+ccb012100@users.noreply.github.com>
2023-06-21 14:07:28 -0500

    kmonad: add log level as temp bug fix
    worrafound fohttps://github.com/kmonad/kmonad/issues/453

--- start-kmonad.bat
+++ start-kmonad.bat
@@ -1,3 +1,3 @@
 REM starting kmonad
-
-kmonad %HOMEPATH%\.config\kmonad\dell_latitude.kbd
+REM loglevel is a temporary fix for https://github.com/kmonad/kmonad/issues/394
+kmonad %HOMEPATH%\.config\kmonad\dell_latitude.kbd -l info
