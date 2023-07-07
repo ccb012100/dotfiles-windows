723cdec828a2a56d3a283ad07be1fcf0d8d7d37b
Christopher Bocardo <397636+ccb012100@users.noreply.github.com>
2023-07-07 10:45:42 -0500

    kmonad: add Dell Precision config

--- scripts/start-kmonad.bat
+++ scripts/start-kmonad.bat
@@ -1,3 +1,3 @@
 REM starting kmonad
 REM loglevel is a temporary fix for https://github.com/kmonad/kmonad/issues/453
-kmonad %HOMEPATH%\.config\kmonad\dell_latitude.kbd -l info
+kmonad %HOMEPATH%\.config\kmonad\dell_precision.kbd -l info
