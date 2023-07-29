4abbb2fa05e57dc4fd20056c07e2ce6b1975c390
Christopher Bocardo <397636+ccb012100@users.noreply.github.com>
2023-07-29 14:45:56 -0500

    git: fix git bash reg key name

--- scripts/remove-git-bash-context-items.bat
+++ scripts/remove-git-bash-context-items.bat
@@ -1,10 +1,10 @@
 @ECHO OFF
 @REM Note: must run as Administrator
 
+reg delete HKEY_CLASSES_ROOT\Directory\background\shell\git_gui
 reg delete HKEY_CLASSES_ROOT\Directory\background\shell\git_shell
-reg delete HKEY_CLASSES_ROOT\Classes\Directory\background\shell\git_gui
 
-reg delete HKEY_LOCAL_MACHINE\SOFTWARE\Classes\Directory\background\shell\git_shell
 reg delete HKEY_LOCAL_MACHINE\SOFTWARE\Classes\Directory\background\shell\git_gui
+reg delete HKEY_LOCAL_MACHINE\SOFTWARE\Classes\Directory\background\shell\git_shell
 
 @PAUSE
