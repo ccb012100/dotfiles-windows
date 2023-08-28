7e9a444d9ece0affd8b9a0ba8a5a438c366ca644
Christopher Bocardo <397636+ccb012100@users.noreply.github.com>
2023-08-28 11:17:24 -0500

    starship: update config

--- PC/HOMEPATH/.config/bash/tools.sh
+++ PC/HOMEPATH/.config/bash/tools.sh
@@ -45,3 +45,9 @@ eval "$(starship init bash)"
 
 # zoxide
 eval "$(zoxide init bash)"
+
+function set_win_title(){
+    echo -ne "\033]0; $(basename "$PWD") \007"
+}
+
+starship_precmd_user_func="set_win_title"
