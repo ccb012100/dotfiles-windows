8b0523065f149d415f563762e0bd3aee3fd6c23f
Christopher Bocardo <397636+ccb012100@users.noreply.github.com>
2023-10-02 10:52:49 -0500

    bash: formatting

--- PC/HOMEPATH/.config/bash/tools.sh
+++ PC/HOMEPATH/.config/bash/tools.sh
@@ -2,8 +2,7 @@
 
 # dotnet CLI
 #: bash parameter completion for the dotnet CLI
-function _dotnet_bash_complete()
-{
+function _dotnet_bash_complete() {
   local cur="${COMP_WORDS[COMP_CWORD]}" IFS=$'\n' # On Windows you may need to use use IFS=$'\r\n'
   local candidates
 
@@ -20,13 +19,13 @@ complete -f -F _dotnet_bash_complete dotnet
 #: - The first argument to the function ($argv[1]) is the base path to start traversal
 #: - See the source code (completion.{bash,zsh}) for the details.
 function _fzf_compgen_path() {
-    # shellcheck disable=SC2154
-    fd --hidden --follow --exclude ".git" . "${argv[1]}"
+  # shellcheck disable=SC2154
+  fd --hidden --follow --exclude ".git" . "${argv[1]}"
 }
 
 # Use fd to generate the list for directory completion
 function _fzf_compgen_dir() {
-    fd --type d --hidden --follow --exclude ".git" . "${argv[1]}"
+  fd --type d --hidden --follow --exclude ".git" . "${argv[1]}"
 }
 
 export fzf_fd_opts='--hidden' # include hidden files
@@ -35,7 +34,7 @@ export fzf_fd_opts='--hidden' # include hidden files
 #: using https://github.com/PatrickF1/fzf.fish
 export FZF_COMPLETION_TRIGGER=';;'
 export FZF_DEFAULT_COMMAND='fd --type f --strip-cwd-prefix'
-#: dracul theme
+#: dracula theme
 export FZF_DEFAULT_OPTS='--cycle --layout=reverse --info=inline --border --margin=1 --padding=1 --color=fg:#f8f8f2,bg:#282a36,hl:#bd93f9 --color=fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9 --color=info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6 --color=marker:#ff79c6,spinner:#ffb86c,header:#6272a4'
 
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
@@ -57,12 +56,11 @@ source "$HOME/bin/fzf-keybindings.bash"
 # starship
 eval "$(starship init bash)"
 
-function set_win_title(){
-    echo -ne "\033]0; $(basename "$PWD") \007"
+function set_win_title() {
+  echo -ne "\033]0; $(basename "$PWD") \007"
 }
 
 starship_precmd_user_func="set_win_title"
 
 # zoxide
 eval "$(zoxide init bash)"
-
