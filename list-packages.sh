3d1d9abf365af274b056144af3719d384741defa
Christopher Bocardo <397636+ccb012100@users.noreply.github.com>
2023-10-09 15:45:54 -0500

    update scripts

--- list-packages.sh
+++ list-packages.sh
@@ -1,24 +1,14 @@
 #!/usr/bin/env bash
 set -Eeou pipefail
 
-repo=$(dirname -- \"$(readlink -f -- \"\\$0\")\")
+repo=$(dirname -- "$(readlink -f -- "$0")")
 
-msg() {
-  echo >&2 -e "${1-}"
-}
-
-if [[ -t 2 ]] && [[ -z "${NO_COLOR-}" ]] && [[ "${TERM-}" != "dumb" ]]; then
-  NOFORMAT='\033[0m' RED='\033[0;31m' GREEN='\033[0;32m' ORANGE='\033[0;33m' BLUE='\033[0;34m' PURPLE='\033[0;35m' CYAN='\033[0;36m' YELLOW='\033[1;33m'
-else
-  NOFORMAT='' RED='' GREEN='' ORANGE='' BLUE='' PURPLE='' CYAN='' YELLOW=''
-fi
-
-msg "${ORANGE}Updating chocolatey package list...${NOFORMAT}"
+echo 'Updating chocolatey package list...'
 
 /mnt/c/Windows/SysWOW64/cmd.exe /c choco list >|"$repo"/applications/chocolatey/packages.txt
 
-msg "${ORANGE}Updating winget package list...${NOFORMAT}"
+echo 'Updating winget package list...'
 
 /mnt/c/Windows/SysWOW64/cmd.exe /c winget list --disable-interactivity >|"$repo"/applications/winget/packages.txt
 
-msg "${GREEN}Completed successfully${NOFORMAT}"
+echo 'Finished!'
--- rsync-to-repo-and-format.sh
+++ rsync-to-repo-and-format.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
+set -Eeou pipefail
+
 git_dir=$(dirname -- "$(readlink -f -- "$0")")
 win_homepath="$HOME/win" # `~/win` is symlinked to `/mnt/c/Users/<user_name>`
 repo_homepath="$git_dir/PC/HOMEPATH"
 
+echo 'Syncing to repository...'
 # --verbose --dry-run \
-
 rsync --relative --dirs --recursive --times --progress \
     --files-from="$git_dir"/write-to-local/files-to-sync-from-home "$win_homepath/" "$repo_homepath"
 
+echo 'Formatting...'
+
 "$git_dir"/format-files.sh
+
+echo 'Finished!'
